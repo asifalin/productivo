45133aad86664d264815b4ca56e6c9f7c646ac00
Asif Ali <99185698+asifalin@users.noreply.github.com>
2024-02-28 14:28:23 +0500

    Dashbord (#2)
    * sidebar complite
    
    * navbar
    
    * Change
    
    * Head complete
    
    ---------
    
    Co-authored-by: asifalin <asifalinagari55@gmail.com>

---
+++ Components/Dashbord/Dashbord.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Head from './Head'
+
+
+function Dashbord() {
+  return (
+    <div>
+      <Navbar/>
+      <Head/>
+    </div>
+  )
+}
+
+export default Dashbord
--- app/Dashbord/page.tsx
+++ app/Dashbord/page.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
 import Sidebar from '@/Components/Sidebar/Sidebar';
+import Dashboard from '../../Components/Dashbord/Dashbord';
 
-function Dashbord() {
+function DashboardPage() {
   return (
     <div className='flex'>
       <div className='bg-[#004AAD] w-[231px] h-[1628px] text-white flex'>
-        <Sidebar/>
+        <Sidebar />
       </div>
-          <div className="bg-[#FFFFFF]  w-full h-screen">
-
-</div>
+      <div className="bg-[#FFFFFF] w-full ">
+        <Dashboard />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashbord ;
+export default DashboardPage;
