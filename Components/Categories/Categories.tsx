bae82a36523442b654b3cae7052379fbafba6e73
asifalin <asifalinagari55@gmail.com>
2024-03-01 16:51:22 +0500

    categories

---
+++ Components/Categories/Categories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Dashbord/Navbar";
+
+function Categories() {
+  return (
+    <>
+      <div className="flex items-center w-full justify-between">
+        <h1>Categories</h1>
+        <Navbar/>
+      </div>
+    </>
+  );
+}
+
+export default Categories;
