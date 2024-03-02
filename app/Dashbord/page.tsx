02c7f148052e9f2036a55067001183c7e4769f6c
asifalin <asifalinagari55@gmail.com>
2024-03-02 14:48:33 +0500

    Resole conflite

--- app/Dashbord/page.tsx
+++ app/Dashbord/page.tsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import React from 'react'
-import Sidebar from '@/Components/Sidebar/Sidebar';
-=======
 import React from 'react';
 import Sidebar from '@/Components/Sidebar/Sidebar';
 import Dashboard from '../../Components/Dashbord/Dashbord';
->>>>>>> db9d71244f872750aafb061ff30293bcde978286
 
 function DashboardPage() {
   return (
@@ -16,12 +11,6 @@ function DashboardPage() {
       <div className="bg-[#FFFFFF] w-full ">
         <Dashboard />
       </div>
-<<<<<<< HEAD
-          <div className="bg-[#FFFFFF]  w-full h-screen">
-
-</div>
-=======
->>>>>>> db9d71244f872750aafb061ff30293bcde978286
     </div>
   );
 }
